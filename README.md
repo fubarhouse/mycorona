9cb173866af56c763a8e1469e5d163bbe58b40b1
Karl Hepworth <karl.hepworth@gmail.com>
2020-04-13 23:06:10 +1000

    initial commit
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# Polybar: Corona Stats
+
+WIP Polybar script that will show you local statistics from the global datasets
+
+## Todo:
+
+Get a working configuration example
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var (
+	confirmURL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+	deathURL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+	recoverURL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
+
+	locationFlag string
+)
+
+func getData(url string) string {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+
+	for _, v := range strings.Split(string(body), "\n") {
+		if strings.Contains(v, locationFlag) {
+			d := strings.Split(v, ",")
+			return d[len(d) - 1]
+		}
+	}
+
+	return ""
+}
+
+func main() {
+
+	var confirmedFlag bool
+	var deadFlag bool
+	var recoveredFlag bool
+
+	flag.BoolVar(&confirmedFlag, "c", true, "Specify username. Default is root")
+	flag.BoolVar(&deadFlag, "d", true, "Specify pass. Default is password")
+	flag.BoolVar(&recoveredFlag, "r", true, "Specify pass. Default is password")
+	flag.StringVar(&locationFlag, "l", "", "Specify pass. Default is password")
+
+	flag.Parse()
+
+	if locationFlag == "" {
+		fmt.Println("ERROR")
+		os.Exit(1)
+	}
+
+	confirmed := getData(confirmURL)
+	dead := getData(deathURL)
+	recovered := getData(recoverURL)
+
+	if confirmedFlag {
+		fmt.Print(" C ", confirmed)
+	}
+	if deadFlag {
+		fmt.Print(" D ", dead)
+	}
+	if recoveredFlag {
+		fmt.Print(" R ", recovered)
+	}
+	return
+}
